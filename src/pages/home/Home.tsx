7ef5fe3f73be320947648a4b661bfa254008be39
akashindex <itz.asdf303@gmail.com>
2024-10-29 03:02:40 +0500

    first commit

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,8 +4,9 @@ import "./Home.scss";
 export function Home() {
   return (
     <div className="home">
-      <h1>Hi, I am Akash Ali</h1>
-      <p>A Software Engineer Enthusiastic on 3D development</p>
+      <h1 className="colgroup-Heading">Hi, I am Akash Ali</h1>
+      <p className="colgroup-text">A Software Engineer Enthusiastic on 3D development</p>
+      <button className="btnn">Lets connect</button>
     </div>
   );
 }
