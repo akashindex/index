4d62129433436fcd40579221f1cd3901ffe47082
akashindex <itz.asdf303@gmail.com>
2024-10-30 20:00:33 +0500

    cha\s

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -8,7 +8,7 @@ export function Home() {
       <div className="home-about">
         <h1 className="colgroup-Heading">Hi, I am Akash Ali</h1>
         <p className="colgroup-text">A Software Engineer Enthusiastic on 3D development</p>
-        <button className="btnn">Lets connect</button>
+        <button className="btnn-slash">Lets connect</button>
       </div>
       <div className="home-main">
         <div className="cards-about">
@@ -27,7 +27,7 @@ export function Home() {
               <img src={img} alt="" />
             </span>
             <span className="text">
-              <h2 className="card-heading">Website Design</h2>
+              <h2 className="card-heading">Front-End development</h2>
               <p className="colgroup-text">Our approach to website design is to create a website that strengthens your company’s brand while ensuring ease of use and simplicity for your audience.</p>
             </span>
             <button className="btnn-slash">learn more</button>
@@ -37,7 +37,7 @@ export function Home() {
               <img src={img} alt="" />
             </span>
             <span className="text">
-              <h2 className="card-heading">Website Design</h2>
+              <h2 className="card-heading">Performance optimization</h2>
               <p  className="colgroup-text">Our approach to website design is to create a website that strengthens your company’s brand while ensuring ease of use and simplicity for your audience.</p>
             </span>
             <button className="btnn-slash">learn more</button>
